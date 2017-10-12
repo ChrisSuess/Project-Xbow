a8dfb293daea29aba3d82860d1c9e6b8032f90ad
Christian Suess <csuess@hotmail.co.uk>
2017-10-12 14:09:55 +0100

    Clean up of repo

--- Mountpoint/diis/mount_command.sh
+++
@@ -1,3 +0,0 @@
-s3fs gromacs . -o passwd_file=~/.aws/passwd_s3fs
-
-sudo sshfs ubuntu@ec2-54-171-179-57.eu-west-1.compute.amazonaws.com:/home/ubuntu/gromacs ~/Xbow/Mountpoint/TEST/ -o IdentityFile=~/Xbow/XBOW-DEMO.pem -o allow_other
