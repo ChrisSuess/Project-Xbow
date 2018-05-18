91049696ad5711596f547c7fc70a7a78c20e143e
Christian Suess <csuess@hotmail.co.uk>
2018-05-18 11:07:46 +0100

    Changed way versioning was controlled

--- xbow/setup.py
+++ xbow/setup.py
@@ -72,7 +72,7 @@ class Extension(Ext, object):
 
 setup_args = {
     'name':             "xbow",
-    'version':          "0.3.9",
+    'version':          verstr,
     'description':      "Longbow in the cloud",
     'long_description': "A system to ease the use of cloud resources for MD simulations.",
     'author':          "Christian Suess, Charlie Laughton",
