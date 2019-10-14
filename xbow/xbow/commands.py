cce8c1a3761122e54669d675ea5aae227c65380c
Christian Suess <csuess@hotmail.co.uk>
2019-10-14 08:09:57 +0100

    functions to boot experiment cluster

--- xbow/xbow/commands.py
+++ xbow/xbow/commands.py
@@ -334,3 +334,200 @@ def xbow_bs_settings():
     newfile.write('cluster: delete')
     
     newfile.close()
+
+def configure_pem_keys(region=None, force=False):
+    """
+    Configure xbow by creating the two key pairs for use with scheduler_name
+    and worker_pool_name defined in .xbow/settings.yml
+    """
+    
+    cwd = os.getcwd()
+    xbowdir = cwd + '/.xbow'
+    xbowschd = xbowdir + '/xbow-schd.yml'
+    cfg_file = xbowschd
+
+    with open(cfg_file, 'r') as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
+        
+    ec2 = boto3.resource('ec2', region_name=region)
+    keynames = [cfg['scheduler_name'], cfg['worker_pool_name']]
+    
+    #keynames = [cfg['scheduler_name']]
+    #if not os.path.exists(xbow.XBOW_CONFIGDIR):
+    #    os.mkdir(xbow.XBOW_CONFIGDIR)
+    for keyname in keynames:
+        try:
+            response = ec2.meta.client.describe_key_pairs(KeyNames=[keyname])
+            exists = True
+        except:
+            exists = False
+        if exists:
+            #if not force:
+            #    raise ValueError('The private key {} already exists'.format(keyname))
+            #else:
+            kp = ec2.KeyPair(keyname)
+            kp.delete()
+        response = ec2.meta.client.create_key_pair(KeyName=keyname)
+        pem_file = os.path.join(xbowdir, keyname) + '.pem'
+        with open(pem_file, 'w') as f:
+            f.write(response['KeyMaterial'])
+        os.chmod(pem_file, 0o600)
+
+def create_exp():
+    """
+    Create a 'lab' - head node in the cloud
+    """
+    
+    cwd = os.getcwd()
+    xbowdir = cwd + '/.xbow'
+    
+    if not os.path.exists(xbowdir):
+        create_exp_settings()
+        
+    xbowschd = xbowdir + '/xbow-schd.yml'
+
+    cfg_file = xbowschd
+
+    with open(cfg_file, 'r') as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
+    
+    configure_pem_keys()
+
+    schedulers = instances.get_by_name(cfg['scheduler_name'])
+    if len(schedulers) > 1:
+        print('Your Lab is already running.')
+        exit(1)
+
+    fs_id = filesystems.fs_id_from_name(cfg['shared_file_system'], 
+                                        region=cfg['region'], 
+                                        ) 
+    
+    if fs_id is None:
+        print('A shared cloud filesystem does not exist')
+        print('Creating a filesystem, this may take a moment...')
+        fs_id = filesystems.create_fs(cfg['shared_file_system'],
+                                     region=cfg['region'], 
+                                     efs_security_groups=cfg['efs_security_groups']
+                                    )
+
+    cfg['fs_id'] = fs_id
+
+    if len(schedulers) == 1:
+        inst = schedulers[0]
+        print('Scheduler already running')
+    else:
+        print("Starting the scheduler node - this may take some time...")
+        
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('-s', '--script', help='name of provisioning script')
+    #parser.add_argument('-n', '--n_workers', help='number of workers to launch')
+
+    #args = parser.parse_args()
+
+    preamble_data = '''Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+'''
+    scheduler_user_data='''
+#!/bin/bash
+mkdir -p /run/metadata/xbow
+touch /run/metadata/xbow/is_scheduler
+echo 'XBOW_SHARED_FILESYSTEM={fs_id}.efs.{region}.amazonaws.com:/' > /run/metadata/xbow/shared_file_system
+echo 'SHARED={mount_point}' >> /etc/environment
+'''.format(**cfg)
+
+    scheduler_extra_data = ''
+    worker_extra_data = ''
+    """
+    if args.script:
+        with open(args.script, 'r') as f:
+            for_scheduler = True
+            for_worker = True
+            for line in f:
+                if len(line) == 0:
+                    pass
+                elif line[0] == '#':
+                    pass
+                elif line[0] == '[':
+                    if '[all]' in line:
+                        for_scheduler = True
+                        for_worker = True
+                    elif [scheduler] in line:
+                        for_scheduler = True
+                        for_worker = False
+                    elif '[workers]' in line:
+                        for_scheduler = False
+                        for_worker = True
+                    else:
+                        raise ValueError('Error - unrecognised command in provisioning file')
+                else:
+                    if for_scheduler:
+                        scheduler_extra_data += line
+                    if for_worker:
+                        worker_extra_data += line
+    """
+
+    final_data = '''
+
+--//'''
+    
+    if len(schedulers) == 1:
+        inst = schedulers[0]
+        print('Scheduler already running, now starting workers. This may take some time...')
+    else:
+        print("Starting the scheduler node - this may take some time...")
+
+        user_data = preamble_data + scheduler_user_data + scheduler_extra_data + final_data
+        inst = instances.create(
+                                cfg['scheduler_name'],
+                                image_id=cfg['image_id'],
+                                instance_type=cfg['scheduler_instance_type'],
+                                ec2_security_groups = cfg['ec2_security_groups'],
+                                user_data=user_data
+                               )
+        print("Scheduler ready, now starting workers. This may take some time...")
+    
+    cfg['scheduler_ip_address'] = inst.private_ip_address
+    if not 'worker_nprocs' in cfg:
+        cfg['worker_nprocs'] = 1
+
+    worker_user_data = '''
+#!/bin/bash
+mkdir -p /run/metadata/xbow
+echo 'XBOW_SCHEDULER_IP_ADDRESS={scheduler_ip_address}' > /run/metadata/xbow/scheduler_ip_address
+echo 'XBOW_WORKER_NPROCS={worker_nprocs}' >> /run/metadata/xbow/scheduler_ip_address
+echo 'XBOW_SHARED_FILESYSTEM={fs_id}.efs.{region}.amazonaws.com:/' > /run/metadata/xbow/shared_file_system
+echo 'SHARED={mount_point}' >> /etc/environment
+'''.format(**cfg)
+
+    """
+    if args.n_workers:
+        n_workers = int(args.n_workers)
+    else:
+        n_workers = cfg['pool_size']
+    """    
+
+    user_data = preamble_data + worker_user_data + worker_extra_data + final_data
+    sip = pools.create_spot_pool(cfg['worker_pool_name'],
+                            count=cfg['pool_size'],
+                            price=cfg['price'],
+                            image_id=cfg['image_id'],
+                            instance_type=cfg['worker_instance_type'],
+                            ec2_security_groups=cfg['ec2_security_groups'],
+                            user_data=user_data
+                            )
