f7cf3f717962f3df1f6bf28793cbf76a690074b1
Christian Suess <csuess@hotmail.co.uk>
2019-11-01 11:43:10 +0000

    Hashed out unneeded commands

--- xbow/xbow/commands.py
+++ xbow/xbow/commands.py
@@ -82,7 +82,7 @@ Content-Transfer-Encoding: 7bit
 Content-Disposition: attachment; filename="userdata.txt"
 
 #!/bin/bash
-pip install dask distributed && sudo -u ubuntu dask-scheduler > /home/ubuntu/scheduler.log 2>&1 &
+#pip install dask distributed && sudo -u ubuntu dask-scheduler > /home/ubuntu/scheduler.log 2>&1 &
 mkdir -p {mount_point}
 mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 {fs_id}.efs.{region}.amazonaws.com:/ {mount_point}
 chmod go+rw {mount_point}
